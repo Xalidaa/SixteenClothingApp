145337abe3408a9f6af05d58ca503961d500db53
Xalidaa <hacizadexalide@gmail.com>
2026-01-05 10:30:43 +0400

    Add Front To Back

---
+++ SixteenClothing/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SixteenClothing.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
